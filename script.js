c4dcefc33524e5f17e844d3c50b0a83e84cb3090
Ailton <junior.peron@hotmail.com>
2021-07-28 18:38:58 -0300

    projeto final

--- script.js
+++ script.js
@@ -1,9 +1,67 @@
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+let isJumping = false;
+let position =0;
 
 function handleKeyUp(event) {
     if(event.keyCode === 32) {
-        console.log('Pressionou o espaço');
+        if(!isJumping) {
+            jump();
+        }
     }
 }
 
+function jump() {
+    isJumping =true;
+
+    let upInterval = setInterval(() => {
+        if(position >= 150 ) {
+            clearInterval(upInterval);
+
+            //descendo
+            let downInterval = setInterval(() => {
+                if(position<= 0) {
+                    clearInterval(downInterval);
+                    isJumping = false;
+                } else {   
+                    position -= 20;
+                    dino.style.bottom = position + 'px';
+                }
+            },20);
+        } else {
+            //subindo
+            position += 20;
+            dino.style.bottom = position + 'px';
+        }
+    },20);
+}
+
+function createCactus() {
+    const cactus = document.createElement('div');
+    let cactusPostition = 1000;
+    let randomTime = Math.random() * 6000;
+
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000+'px';
+    background.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+        if(cactusPostition < -60) {
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        } else if(cactusPostition> 0 && cactusPostition < 60 && position < 60){
+            //gameover
+            document.body.innerHTML = '<h1 class="game-over">Fim de Jogo</h1>';
+            clearInterval(leftInterval);
+            
+        } else {
+            cactusPostition -=10;
+            cactus.style.left = cactusPostition + 'px';
+        }
+    },20);
+
+    setTimeout(createCactus,randomTime);
+}
+
+createCactus();
 document.addEventListener('keyup', handleKeyUp);
